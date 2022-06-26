d839743f1aa119f81c5f054a555e1d49f66774ff
Zenon <vandalzenon@gmail.com>
2022-06-26 18:24:53 +0800

    added card

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Calendar from "./Components/Calendar";
 import Patient from "./Components/Patient";
 import Driver from "./Components/Drivers";
 import Navbar from "./Components/navbar";
-import {Row, Col} from 'react-bootstrap';
+import {Row,Card, Col} from 'react-bootstrap';
 
 function App() {
   return (
@@ -14,13 +14,15 @@ function App() {
       <div className="container ">
         <Row>
           
-         <Col md={3} xl={3}> <Patient/></Col>
+         <Col md={3} xl={3}> <Card className="p-0"><Patient/></Card></Col>
          <Col md={6} xl={6}>
-            <div className="calendarContainer">
-              <Calendar />
-            </div>
+            <Card  className="p-2">
+              <div className="calendarContainer">
+                <Calendar />
+              </div>
+            </Card>
          </Col>
-         <Col md={3} xl={3}> <Driver/></Col>
+         <Col md={3} xl={3}><Card  className="p-2"> <Driver/></Card></Col>
           
         </Row>
       </div>
